da88d98746a027a28968d7831276bb55a42ed4f9
christos <paschalidi@outlook.com>
2019-04-13 13:10:18 +0200

    global style

---
+++ src/styles/global.jsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  html {
+    text-rendering: optimizeLegibility;
+    overflow-x: hidden;
+    box-sizing: border-box;
+    -ms-overflow-style: scrollbar;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    background-color: #161719;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: #fff382;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #fa7ea8;
+  }
+`
+
+export default GlobalStyle
